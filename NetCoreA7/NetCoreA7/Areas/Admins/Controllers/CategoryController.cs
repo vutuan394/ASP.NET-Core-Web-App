698f2f72caa2cca7b40d08a8d34174eb76477b9a
vutuan394 <138289477+vutuan394@users.noreply.github.com>
2025-09-26 22:09:30 +0700

    Lab7

---
+++ NetCoreA7/NetCoreA7/Areas/Admins/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreA7.Areas.Admins.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
